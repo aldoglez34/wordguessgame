a31a1cf2cd64d8b2451fedd58fe508dad0e6013f
Aldo Solano González <aldoglez34@gmail.com>
2019-04-05 19:39:20 -0600

    msg

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,7 +37,8 @@ var _word = [];
 // array that's gonna hold the error letters
 var errorLetters = [];
 // default game values
-var difficulty = "EASY";
+var category;
+var difficulty;
 var hearts = 7;
 var points;
 var username;
@@ -75,6 +76,8 @@ $("._newgamebttn").click(function () {
     $("#ui_hearts").html("");
     $("#usedletters").html("");
 
+    cleanLetters();
+
     var canStart = true;
 
     // set username in the global variable
@@ -103,38 +106,19 @@ $("._newgamebttn").click(function () {
         $("#ui_points").html(points);
 
         // set wordsArray depending on the difficulty
-        if (difficulty === "Easy") {
-            wordsArray.push("WHEEL");
-            wordsArray.push("BREAD");
-            wordsArray.push("ANGRY");
-            wordsArray.push("DRESS");
-            wordsArray.push("MILK");
-            wordsArray.push("TABLE");
-            wordsArray.push("POWER");
-            wordsArray.push("LAUGH");
-            hearts = 7;
-        }
-        else if (difficulty === "Medium") {
-            wordsArray.push("ABROAD");
-            wordsArray.push("ACTRESS");
-            wordsArray.push("AIRLINE");
-            wordsArray.push("BACKGROUND");
-            wordsArray.push("BENEATH");
-            wordsArray.push("BREAKFAST");
-            wordsArray.push("CAPTURE");
-            wordsArray.push("PERCENTAGE");
-            hearts = 6;
-        }
-        else if (difficulty === "Hard") {
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            wordsArray.push("FURTHERMORE");
-            hearts = 5;
+        switch (difficulty) {
+            case "Easy":
+                hearts = 7;
+                populateEasyArray();
+                break;
+            case "Medium":
+                hearts = 6;
+                populateMediumArray();
+                break;
+            case "Hard":
+                hearts = 6;
+                populateHardArray();
+                break;
         }
 
         // show hearts
@@ -146,9 +130,6 @@ $("._newgamebttn").click(function () {
         pickNewWord();
         refreshWord();
 
-        console.log(word);
-        console.log(_word);
-
         // hide and show containers  
         $("#newgamecontainer").hide();
         $("#hangmangamecontainer").show(500);
@@ -156,33 +137,6 @@ $("._newgamebttn").click(function () {
     else if (!canStart) {
 
         alert("Enter a username to start the game.")
-    }
-});
-
-// -------------------------------------- language and difficulty listeners
-
-$(".jsdiff").click(function () {
-
-    // clear all options
-    $(".jsdiff").attr("class", "_textopt jsdiff");
-
-    // save this to var
-    var difficultyobj = $(this);
-
-    // assign new class
-    difficultyobj.attr("class", "_textopt _selectedtextopt jsdiff");
-
-    // update global variables
-    difficulty = difficultyobj.text();
-
-    if (difficulty === "EASY") {
-        hearts = 7;
-    }
-    else if (difficulty === "MEDIUM") {
-        hearts = 6;
-    }
-    else if (difficulty === "HARD") {
-        hearts = 5;
     }
 });
 
@@ -196,11 +150,23 @@ document.onkeyup = function (event) {
         // var "k" will hold the keyboard input
         var k = event.key.toUpperCase();
 
+        // mark the letter
+        $("#letterstable tr").each(function () {
+
+            $(this).find("td").each(function () {
+
+                if (k === $(this).text()) {
+
+                    $(this).html("<s class='text-danger'>" + k + "</s>");
+                }
+            })
+        })
+
         // check if the given letter is in the word array
         checkLetter(k);
 
-        // check if the user won or lost
-        checkIfWinOrLose();
+        // wait .5 seconds to check if the user won or lost
+        setTimeout(checkIfWinOrLose, 500);
     }
 }
 
@@ -208,13 +174,13 @@ $(".letter").click(function () {
 
     // var "k" will hold the letter pressed
     var k = $(this).text();
-    $(this).html("<s>" + k + "</s>");
+    $(this).html("<s class='text-danger'>" + k + "</s>");
 
     // check if the given letter is in the word array
     checkLetter(k);
 
-    // check if the user won or lost
-    checkIfWinOrLose();
+    // wait .5 seconds to check if the user won or lost
+    setTimeout(checkIfWinOrLose, 500);
 
 });
 
@@ -228,12 +194,14 @@ function pickNewWord() {
     errorLetters = [];
 
     // generate random number
-    var length = wordsArray.length;
+    var length = 40;
     var random = Math.floor((Math.random() * length) + 1);
 
     // convert word into an array
-    // (substract 1 en case random number is equals to the array length)
-    var w = wordsArray[random - 1];
+    category = wordsArray["w" + random][0]["cat"];
+    var w = wordsArray["w" + random][0]["word"];
+
+    console.log(w);
 
     // the var "word" contains the word picked from the array
     word = w.split('');
@@ -249,6 +217,7 @@ function refreshWord() {
 
     // clear the word div
     $("#theworddiv").html("");
+    $("#category").text(category);
 
     // iterate the duplicated word array and set new word
     for (var i = 0; i < _word.length; i++) {
@@ -361,6 +330,25 @@ function checkIfWinOrLose() {
         points = points + 1000;
         $("#ui_points").html(points);
 
+        cleanLetters();
+
+        // refresh hearts
+        switch (difficulty) {
+            case "Easy":
+                hearts = 7;
+                break;
+            case "Medium":
+                hearts = 6;
+                break;
+            case "Hard":
+                hearts = 6;
+                break;
+        }
+        $("#ui_hearts").html("");
+        for (var i = 0; i < hearts; i++) {
+            $("#ui_hearts").append("<i class='fas fa-heart text-light mr-1'></i>");
+        }
+
         // show new word
         pickNewWord();
         refreshWord();
@@ -379,6 +367,19 @@ function checkIfWinOrLose() {
 
         storageScoreonDB();
     }
+}
+
+function cleanLetters() {
+
+    $("#letterstable tr").each(function () {
+
+        $(this).find("td").each(function () {
+
+            var l = $(this).text();
+            $(this).html("");
+            $(this).html(l);
+        })
+    })
 }
 
 function validateIfLetter(k) {
@@ -407,4 +408,522 @@ function storageScoreonDB() {
         db_level: level,
         db_points: points
     });
+}
+
+// -------------------------------------- arrays
+
+function populateEasyArray() {
+
+    wordsArray = [];
+    var array = {
+        w1: [{
+            cat: "Animals in a Zoo",
+            word: "LION",
+        }],
+        w2: [{
+            cat: "Animals in a Zoo",
+            word: "TIGER",
+        }],
+        w3: [{
+            cat: "Animals in a Zoo",
+            word: "EAGLE",
+        }],
+        w4: [{
+            cat: "Animals in a Zoo",
+            word: "BEAR",
+        }],
+        w5: [{
+            cat: "Animals in a Zoo",
+            word: "WOLF",
+        }],
+        w6: [{
+            cat: "Animals in a Zoo",
+            word: "CAMEL",
+        }],
+        w7: [{
+            cat: "Clothes",
+            word: "PANTS",
+        }],
+        w8: [{
+            cat: "Clothes",
+            word: "SHIRT",
+        }],
+        w9: [{
+            cat: "Clothes",
+            word: "SUIT",
+        }],
+        w10: [{
+            cat: "Clothes",
+            word: "SKIRT",
+        }],
+        w11: [{
+            cat: "Clothes",
+            word: "JEANS",
+        }],
+        w12: [{
+            cat: "Sports",
+            word: "GOLF",
+        }],
+        w13: [{
+            cat: "Sports",
+            word: "BASKETBALL",
+        }],
+        w14: [{
+            cat: "Sports",
+            word: "BASEBALL",
+        }],
+        w15: [{
+            cat: "Sports",
+            word: "SOCCER",
+        }],
+        w16: [{
+            cat: "Sports",
+            word: "FOOTBALL",
+        }],
+        w17: [{
+            cat: "Body",
+            word: "NOSE",
+        }],
+        w18: [{
+            cat: "Body",
+            word: "HEART",
+        }],
+        w19: [{
+            cat: "Body",
+            word: "EAR",
+        }],
+        w20: [{
+            cat: "Body",
+            word: "NECK",
+        }],
+        w21: [{
+            cat: "Body",
+            word: "EYE",
+        }],
+        w22: [{
+            cat: "Months",
+            word: "JANUARY",
+        }],
+        w23: [{
+            cat: "Months",
+            word: "FEBRUARY",
+        }],
+        w24: [{
+            cat: "Months",
+            word: "JULY",
+        }],
+        w25: [{
+            cat: "Months",
+            word: "DECEMBER",
+        }],
+        w26: [{
+            cat: "Months",
+            word: "SEPTEMBER",
+        }],
+        w27: [{
+            cat: "Family Members",
+            word: "COUSIN",
+        }],
+        w28: [{
+            cat: "Family Members",
+            word: "AUNT",
+        }],
+        w29: [{
+            cat: "Family Members",
+            word: "SISTER",
+        }],
+        w30: [{
+            cat: "Family Members",
+            word: "UNCLE",
+        }],
+        w31: [{
+            cat: "Family Members",
+            word: "MOTHER",
+        }],
+        w32: [{
+            cat: "Insects",
+            word: "MOSQUITO",
+        }],
+        w33: [{
+            cat: "Insects",
+            word: "BEE",
+        }],
+        w34: [{
+            cat: "Insects",
+            word: "SPIDER",
+        }],
+        w35: [{
+            cat: "Insects",
+            word: "ANT",
+        }],
+        w36: [{
+            cat: "Insects",
+            word: "FLEA",
+        }],
+        w37: [{
+            cat: "Jobs",
+            word: "ACTOR",
+        }],
+        w38: [{
+            cat: "Jobs",
+            word: "DOCTOR",
+        }],
+        w39: [{
+            cat: "Jobs",
+            word: "PILOT",
+        }],
+        w40: [{
+            cat: "Jobs",
+            word: "DENTIST",
+        }],
+        w41: [{
+            cat: "Jobs",
+            word: "LAWYER",
+        }]
+    };
+    wordsArray = JSON.parse(JSON.stringify(array));
+}
+
+function populateMediumArray() {
+
+    wordsArray = [];
+    var array = {
+        w1: [{
+            cat: "Animals in a Zoo",
+            word: "CHEETAH",
+        }],
+        w2: [{
+            cat: "Animals in a Zoo",
+            word: "LEOPARD",
+        }],
+        w3: [{
+            cat: "Animals in a Zoo",
+            word: "KANGAROO",
+        }],
+        w4: [{
+            cat: "Animals in a Zoo",
+            word: "CROCODILE",
+        }],
+        w5: [{
+            cat: "Animals in a Zoo",
+            word: "ZEBRA",
+        }],
+        w6: [{
+            cat: "Animals in a Zoo",
+            word: "GIRAFFE",
+        }],
+        w7: [{
+            cat: "Clothes",
+            word: "SOCKS",
+        }],
+        w8: [{
+            cat: "Clothes",
+            word: "PAJAMAS",
+        }],
+        w9: [{
+            cat: "Clothes",
+            word: "SCARF",
+        }],
+        w10: [{
+            cat: "Clothes",
+            word: "JACKET",
+        }],
+        w11: [{
+            cat: "Clothes",
+            word: "GLOVES",
+        }],
+        w12: [{
+            cat: "Sports",
+            word: "HOCKEY",
+        }],
+        w13: [{
+            cat: "Sports",
+            word: "BOWLING",
+        }],
+        w14: [{
+            cat: "Sports",
+            word: "RUGBY",
+        }],
+        w15: [{
+            cat: "Sports",
+            word: "SWIMMING",
+        }],
+        w16: [{
+            cat: "Sports",
+            word: "VOLLEYBALL",
+        }],
+        w17: [{
+            cat: "Body",
+            word: "CHEEK",
+        }],
+        w18: [{
+            cat: "Body",
+            word: "ELBOW",
+        }],
+        w19: [{
+            cat: "Body",
+            word: "STOMACH",
+        }],
+        w20: [{
+            cat: "Body",
+            word: "KNEES",
+        }],
+        w21: [{
+            cat: "Body",
+            word: "ANKLE",
+        }],
+        w22: [{
+            cat: "Cars",
+            word: "TOYOTA",
+        }],
+        w23: [{
+            cat: "Cars",
+            word: "HONDA",
+        }],
+        w24: [{
+            cat: "Cars",
+            word: "AUDI",
+        }],
+        w25: [{
+            cat: "Cars",
+            word: "FERRARI",
+        }],
+        w26: [{
+            cat: "Cars",
+            word: "BENTLEY",
+        }],
+        w27: [{
+            cat: "Celebrations",
+            word: "WEDDING",
+        }],
+        w28: [{
+            cat: "Celebrations",
+            word: "CHRISTMAS",
+        }],
+        w29: [{
+            cat: "Celebrations",
+            word: "HALLOWEEN",
+        }],
+        w30: [{
+            cat: "Celebrations",
+            word: "ANNIVERSARY",
+        }],
+        w31: [{
+            cat: "Celebrations",
+            word: "GRADUATION",
+        }],
+        w32: [{
+            cat: "Insects",
+            word: "BEETLE",
+        }],
+        w33: [{
+            cat: "Insects",
+            word: "COCKROACH",
+        }],
+        w34: [{
+            cat: "Insects",
+            word: "CRICKET",
+        }],
+        w35: [{
+            cat: "Insects",
+            word: "WASP",
+        }],
+        w36: [{
+            cat: "Insects",
+            word: "MOTH",
+        }],
+        w37: [{
+            cat: "Jobs",
+            word: "SALESMAN",
+        }],
+        w38: [{
+            cat: "Jobs",
+            word: "WAITER",
+        }],
+        w39: [{
+            cat: "Jobs",
+            word: "ENGINEER",
+        }],
+        w40: [{
+            cat: "Jobs",
+            word: "PROFESSOR",
+        }],
+        w41: [{
+            cat: "Jobs",
+            word: "NURSE",
+        }]
+    };
+    wordsArray = JSON.parse(JSON.stringify(array));
+}
+
+function populateHardArray() {
+
+    wordsArray = [];
+    var array = {
+        w1: [{
+            cat: "Animals in a Zoo",
+            word: "LLAMA",
+        }],
+        w2: [{
+            cat: "Animals in a Zoo",
+            word: "ANTEATER",
+        }],
+        w3: [{
+            cat: "Animals in a Zoo",
+            word: "GORILLA",
+        }],
+        w4: [{
+            cat: "Animals in a Zoo",
+            word: "HYENA",
+        }],
+        w5: [{
+            cat: "Animals in a Zoo",
+            word: "ARMADILLO",
+        }],
+        w6: [{
+            cat: "Animals in a Zoo",
+            word: "OSTRICH",
+        }],
+        w7: [{
+            cat: "Clothes",
+            word: "BOXER",
+        }],
+        w8: [{
+            cat: "Clothes",
+            word: "VEST",
+        }],
+        w9: [{
+            cat: "Clothes",
+            word: "JUMPER",
+        }],
+        w10: [{
+            cat: "Clothes",
+            word: "SLIPPERS",
+        }],
+        w11: [{
+            cat: "Clothes",
+            word: "TROUSERS",
+        }],
+        w12: [{
+            cat: "Sports",
+            word: "CYCLING",
+        }],
+        w13: [{
+            cat: "Sports",
+            word: "SOFTBALL",
+        }],
+        w14: [{
+            cat: "Transportation",
+            word: "SCOOTER",
+        }],
+        w15: [{
+            cat: "Transportation",
+            word: "AMBULANCE",
+        }],
+        w16: [{
+            cat: "Transportation",
+            word: "TAXI",
+        }],
+        w17: [{
+            cat: "Body",
+            word: "FINGERS",
+        }],
+        w18: [{
+            cat: "Body",
+            word: "BEARD",
+        }],
+        w19: [{
+            cat: "Body",
+            word: "CHIN",
+        }],
+        w20: [{
+            cat: "Body",
+            word: "WRIST",
+        }],
+        w21: [{
+            cat: "Body",
+            word: "MOUSTACHE",
+        }],
+        w22: [{
+            cat: "Car Parts",
+            word: "BRAKE",
+        }],
+        w23: [{
+            cat: "Car Parts",
+            word: "SEAT",
+        }],
+        w24: [{
+            cat: "Car Parts",
+            word: "WHEEL",
+        }],
+        w25: [{
+            cat: "Car Parts",
+            word: "WHINDSHIELD",
+        }],
+        w26: [{
+            cat: "Car Parts",
+            word: "CLUTCH",
+        }],
+        w27: [{
+            cat: "Food",
+            word: "NOODLES",
+        }],
+        w28: [{
+            cat: "Food",
+            word: "CROISSANT",
+        }],
+        w29: [{
+            cat: "Food",
+            word: "SHRIMP",
+        }],
+        w30: [{
+            cat: "Food",
+            word: "KETCHUP",
+        }],
+        w31: [{
+            cat: "Food",
+            word: "BAGEL",
+        }],
+        w32: [{
+            cat: "Park",
+            word: "SWINGS",
+        }],
+        w33: [{
+            cat: "Park",
+            word: "PLAYGROUND",
+        }],
+        w34: [{
+            cat: "Park",
+            word: "PICNIC",
+        }],
+        w35: [{
+            cat: "Park",
+            word: "TIRE",
+        }],
+        w36: [{
+            cat: "Park",
+            word: "SAND",
+        }],
+        w37: [{
+            cat: "Drinks",
+            word: "LIQUOR",
+        }],
+        w38: [{
+            cat: "Drinks",
+            word: "COCKTAIL",
+        }],
+        w39: [{
+            cat: "Drinks",
+            word: "BRANDY",
+        }],
+        w40: [{
+            cat: "Drinks",
+            word: "MILKSHAKE",
+        }],
+        w41: [{
+            cat: "Drinks",
+            word: "WINE",
+        }]
+    };
+    wordsArray = JSON.parse(JSON.stringify(array));
 }
